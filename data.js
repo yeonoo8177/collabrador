d634a8adda9a42ff8d9485278102344678eb6c62
collabrador <yeonoo8177@gmail.com>
2026-06-19 14:14:44 +0900

    콜라브라도 손님용 발매 캘린더 (data.js 자동반영)

---
+++ data.js
@@ -0,0 +1,361 @@
+window.CAL_DATA = {
+ "STARS": [
+  "BAPE × 아디다스",
+  "A Ma Maniére × 나이키",
+  "유니클로 UT × 포켓몬",
+  "크록스 × 슈퍼마리오",
+  "GU × 에반게리온",
+  "포트나이트 × 손흥민",
+  "아식스 × 세실리에반센",
+  "노아 × 타이맥스"
+ ],
+ "POPS": [
+  "귀멸의 칼날 전집중展 × 다날",
+  "토이스토리5 × K리그 부산 팝업",
+  "Calvin Klein × 정국 (CKJK)",
+  "토이스토리5 × 현대백화점 팝업"
+ ],
+ "DATA": [
+  {
+   "d": "2026-06-19",
+   "n": "CGV × 토이스토리5",
+   "c": "IP/캐릭터",
+   "w": "CGV 극장",
+   "p": "관람 특전",
+   "desc": "IMAX·4DX 한정 포스터 증정. 영화관마다 굿즈가 다르다."
+  },
+  {
+   "slug": "basketcase-뉴발란스",
+   "n": "Basketcase × 뉴발란스",
+   "c": "패션",
+   "w": "뉴발란스 글로벌",
+   "p": "약 18만원 예상",
+   "desc": "NB 글로벌 발매 · 풋볼컬처",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/basketcase-뉴발란스/card1.jpg",
+   "d": "2026-06-19"
+  },
+  {
+   "slug": "noah-timex",
+   "n": "노아 × 타이맥스",
+   "c": "패션",
+   "w": "무신사 단독",
+   "p": "365,000원",
+   "desc": "블랙/골드·브라운/스틸 2종. 타원형 케이스에 문페이즈 다이얼.",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/noah-timex/card1.jpg",
+   "d": "2026-06-19"
+  },
+  {
+   "slug": "thisisneverthat-gshock",
+   "n": "디스이즈네버댓 × 지샥",
+   "c": "기타",
+   "w": "디스이즈네버댓 공식몰",
+   "p": "약 18만원 예상",
+   "desc": "디스이즈네버댓 공식몰",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/thisisneverthat-gshock/card1.jpg",
+   "d": "2026-06-19"
+  },
+  {
+   "slug": "uniqlo-risso",
+   "n": "유니클로 × 프란체스코 리소",
+   "c": "패션",
+   "w": "유니클로 매장·공식 온라인",
+   "p": "티셔츠 19,900원 · 블라우스 29,900원 · 원피스 49,900원",
+   "desc": "전 마르니 디렉터 프란체스코 리소와의 2026 써머 캡슐",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/uniqlo-risso/card1.jpg",
+   "d": "2026-06-19"
+  },
+  {
+   "d": "2026-06-21",
+   "n": "포트나이트 × 손흥민",
+   "c": "게임",
+   "w": "포트나이트 아이템샵",
+   "p": "",
+   "desc": "손흥민 인게임 스킨 한정 번들. 1주일만 판매."
+  },
+  {
+   "d": "2026-06-22",
+   "n": "GU × 에반게리온",
+   "c": "IP/캐릭터",
+   "w": "GU 일본·해외직구",
+   "p": "",
+   "desc": "SS26 빈티지 그래픽 컬렉션. 한국 미취급, 일본 전용."
+  },
+  {
+   "d": "2026-06-25",
+   "n": "명조 워더링웨이브 × 사이버펑크 엣지러너",
+   "c": "게임",
+   "w": "명조 인게임",
+   "p": "",
+   "desc": "루시·레베카 콜라보 캐릭터/이벤트 인게임 업데이트."
+  },
+  {
+   "d": "2026-06-27",
+   "n": "BAPE × 아디다스",
+   "c": "스니커",
+   "w": "",
+   "p": "",
+   "desc": "BAPE 카모 × 아디다스 실루엣. 발매 임박."
+  },
+  {
+   "d": "2026-06-27",
+   "n": "JAIDE × 조던",
+   "c": "스니커",
+   "w": "",
+   "p": "",
+   "desc": "여성 디자이너 JAIDE의 조던 협업."
+  },
+  {
+   "d": "2026-06-27",
+   "n": "CGV × KBO",
+   "c": "기타",
+   "w": "CGV",
+   "p": "",
+   "desc": "KBO 시즌 팝콘 버킷·구단 굿즈 콜라보."
+  },
+  {
+   "d": "2026-06-27",
+   "n": "귀멸의 칼날 전집중展 × 다날",
+   "c": "IP/캐릭터",
+   "w": "성수 에스팩토리",
+   "p": "",
+   "desc": "몰입형 체험 전시 + 한정 굿즈. 6/27~9/27."
+  },
+  {
+   "slug": "아식스-세실리에반센",
+   "n": "아식스 × 세실리에반센",
+   "c": "패션",
+   "w": "무신사스토어 성수 단독",
+   "p": "209,000원 (3컬러)",
+   "desc": "무신사스토어 성수 단독",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/아식스-세실리에반센/card1.jpg",
+   "d": "2026-06-27"
+  },
+  {
+   "d": "2026-06-30",
+   "n": "메가박스 × 주술회전",
+   "c": "IP/캐릭터",
+   "w": "메가박스",
+   "p": "",
+   "desc": "주술회전 사멸회유 편 개봉 기념 굿즈."
+  },
+  {
+   "d": "2026-07-01",
+   "n": "A Ma Maniére × 나이키",
+   "c": "스니커",
+   "w": "SNKRS",
+   "p": "",
+   "desc": "하이엔드 스트리트 A Ma Maniére의 나이키 협업."
+  },
+  {
+   "d": "2026-07-01",
+   "n": "토이스토리5 × K리그 부산 팝업",
+   "c": "IP/캐릭터",
+   "w": "부산 (7/1~7/12)",
+   "p": "",
+   "pop": 1,
+   "desc": "K리그 × 디즈니 토이스토리5 부산 팝업. 포토존·한정 굿즈."
+  },
+  {
+   "d": "2026-07-02",
+   "n": "젠레스 존 제로 × 더벤티",
+   "c": "F&B",
+   "w": "더벤티 전국",
+   "p": "",
+   "desc": "인기 게임 ZZZ × 더벤티 음료·컵 콜라보."
+  },
+  {
+   "d": "2026-07-15",
+   "n": "유니클로 UT × YOASOBI",
+   "c": "IP/캐릭터",
+   "w": "유니클로",
+   "p": "",
+   "desc": "일본 인기 듀오 YOASOBI 그래픽 티 컬렉션."
+  },
+  {
+   "d": "2026-07-15",
+   "n": "크록스 × 슈퍼마리오",
+   "c": "IP/캐릭터",
+   "w": "",
+   "p": "",
+   "desc": "마리오 지비츠 + 한정 클로그."
+  },
+  {
+   "d": "2026-07-17",
+   "n": "프리더유스 × 조던",
+   "c": "스니커",
+   "w": "",
+   "p": "",
+   "desc": "Free.99 무드의 조던 협업."
+  },
+  {
+   "d": "2026-07-25",
+   "n": "유니클로 UT × 포켓몬",
+   "c": "패션",
+   "w": "유니클로",
+   "p": "",
+   "desc": "포켓몬 그래픽 UT 신규 컬렉션."
+  },
+  {
+   "d": "2026-09-18",
+   "n": "모왈롤라 × 조던",
+   "c": "스니커",
+   "w": "",
+   "p": "",
+   "desc": "나이지리아계 브랜드 Mowalola × 조던."
+  },
+  {
+   "d": "2026-10-01",
+   "n": "Technics × Fritz Hansen",
+   "c": "리빙",
+   "w": "",
+   "p": "300개 한정",
+   "desc": "SL-40CBT 버건디 한정 턴테이블. 300개 한정."
+  }
+ ],
+ "ONGOING": [
+  {
+   "slug": "t1-zanmang-loopy",
+   "n": "T1 × 잔망루피",
+   "c": "IP/캐릭터",
+   "w": "T1샵 단독 (shop-t1.gg)",
+   "p": "8,500원~108,000원",
+   "desc": "페이커의 전설과 잔망루피의 귀여움이 만났다. 피규어 세트·블라인드 박스·플러시 키링·넥필로우·핀 배지·데스크 매트·키링·스티커까지 9종.",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/t1-zanmang-loopy/card1.jpg",
+   "till": "발매중"
+  },
+  {
+   "slug": "swarovski-47-mlb",
+   "n": "스와로브스키 × '47 × MLB",
+   "c": "패션",
+   "w": "스와로브스키 공식몰·매장 / '47",
+   "p": "약 21만원 예상",
+   "desc": "뉴욕 양키스 NY 로고를 바게트·라운드 컷 크리스탈 100여 개로 세팅한 한정판. 라이트 블루·네이비·라이트 핑크·마시멜로(스와로브스키 단독) 4색.",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/swarovski-47-mlb/card1.jpg",
+   "till": "발매중"
+  },
+  {
+   "slug": "stoneisland-뉴발란스",
+   "n": "Stone Island × 뉴발란스",
+   "c": "패션",
+   "w": "stoneisland.com · 뉴발란스 글로벌",
+   "p": "스니커즈 약 27~30만원 예상 · 의류 별도",
+   "desc": "2026 서머 캡슐 · 풋볼킷 포함",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/stoneisland-뉴발란스/card1.jpg",
+   "till": "발매중"
+  },
+  {
+   "slug": "our-legacy-work-shop-stussy",
+   "n": "Our Legacy Work Shop × Stüssy",
+   "c": "패션",
+   "w": "",
+   "p": "해외 현지가 (국내 판매 없음)",
+   "desc": "⚠️ 국내 미발매 · 해외 한정 (스톡홀름·런던·뉴욕·파리 오프라인)",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/our-legacy-work-shop-stussy/card1.jpg",
+   "till": "발매중"
+  },
+  {
+   "slug": "malbon-arsham",
+   "n": "말본 골프 × 다니엘 아샴",
+   "c": "패션",
+   "w": "말본 골프 공식몰",
+   "p": "티셔츠 약 9만원 예상 ~ 카트백 약 61만원 예상",
+   "desc": "조각가 다니엘 아샴의 침식·균열 미학을 골프웨어로 옮긴 컬렉션. 스텔스 카모와 아샴 그린으로 코스부터 일상까지.",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/malbon-arsham/card1.jpg",
+   "till": "발매중"
+  },
+  {
+   "slug": "humanmade-verdy",
+   "n": "Human Made × Verdy",
+   "c": "패션",
+   "w": "휴먼메이드 잠실(롯데월드몰 1F)",
+   "p": "워크 재킷 700,000원 · 티셔츠 158,000원 · 키링 30,000원",
+   "desc": "잠실 스토어 오픈 한정",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/humanmade-verdy/card1.jpg",
+   "till": "발매중"
+  },
+  {
+   "slug": "gallery-dept-psg",
+   "n": "갤러리 디파트먼트 × 파리 생제르맹",
+   "c": "패션",
+   "w": "LA La Maison 팝업(할리우드 애슬레틱 클럽) + 갤러리 디파트먼트 온라인",
+   "p": "약 34만원 예상 (현재 온라인 품절)",
+   "desc": "프론트 'I Love Paris' 타이포 + 백 PSG 그래픽, 시그니처 워시 페이딩",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/gallery-dept-psg/card1.jpg",
+   "till": "발매중"
+  },
+  {
+   "slug": "evangelion-casetify",
+   "n": "에반게리온 × 케이스티파이",
+   "c": "기타",
+   "w": "케이스티파이 공식몰",
+   "p": "플러그수트 에어팟 맥스 케이스 약 17만원 예상 · A.T. 필드 무선 충전기 약 11만원 예상 · 사도 메탈 체인 스트랩 약 10만원 예상 · 사도 그립 스탠드 약 9만원 예상",
+   "desc": "에반게리온 30주년 두 번째 테크 액세서리 컬렉션",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/evangelion-casetify/card1.jpg",
+   "till": "발매중"
+  },
+  {
+   "slug": "calvinklein-jungkook",
+   "n": "Calvin Klein × 정국",
+   "c": "패션",
+   "w": "성수 팝업 진행 중",
+   "p": "가죽 재킷 1,420,000원",
+   "desc": "정국 직접 디자인 · 20피스 캡슐",
+   "img": "https://raw.githubusercontent.com/yeonoo8177/collabrador-assets/main/calvinklein-jungkook/card1.jpg",
+   "till": "발매중"
+  },
+  {
+   "n": "매지컬 치이카와 팝업",
+   "c": "IP/캐릭터",
+   "till": "~6/25",
+   "w": "아이파크몰 용산 3F",
+   "pop": 1,
+   "desc": "먼작귀 치이카와 마법소녀 콘셉트. 한정 굿즈 + 포토존.",
+   "p": "무료입장"
+  },
+  {
+   "n": "포켓몬 메가페스타 (메타몽 놀이터·시크릿 포레스트)",
+   "c": "IP/캐릭터",
+   "till": "~6/21",
+   "w": "성수동 · 서울숲",
+   "pop": 1,
+   "desc": "포켓몬 30주년. 메타몽 놀이터 + 서울숲 시크릿 포레스트.",
+   "p": "무료입장"
+  },
+  {
+   "n": "케이팝 데몬헌터스 1주년 팝업",
+   "c": "IP/캐릭터",
+   "till": "~6/28",
+   "w": "더현대 대구 B2F",
+   "pop": 1,
+   "desc": "K-pop 데몬헌터스 1주년 기념 팝업.",
+   "p": ""
+  },
+  {
+   "n": "House of Toy Story 팝업",
+   "c": "IP/캐릭터",
+   "till": "~7/12",
+   "w": "렉트하우스 성수 1~4F",
+   "pop": 1,
+   "desc": "토이스토리5 개봉 연계. 4층 규모 굿즈·포토존(무료입장).",
+   "p": "무료입장"
+  },
+  {
+   "n": "농심 신라면 분식 (전시·팝업)",
+   "c": "F&B",
+   "till": "~11/30",
+   "w": "성수동",
+   "pop": 1,
+   "desc": "신라면 테마 분식 체험형 공간. 메뉴 5,000~6,900원.",
+   "p": ""
+  },
+  {
+   "n": "성수동고릴라 페스티벌 VOL.1",
+   "c": "기타",
+   "till": "~6/28",
+   "w": "북성수 일대",
+   "pop": 1,
+   "desc": "북성수 로컬 브랜드 15곳 합동 페스티벌.",
+   "p": "무료"
+  }
+ ]
+};
